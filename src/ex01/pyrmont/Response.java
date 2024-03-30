27542939a567a1d0dfb7a1fff98b9ecbdfaff732
moralok <wrmao.public@outlook.com>
2024-03-30 23:54:10 +0800

    format code

--- src/ex01/pyrmont/Response.java
+++ src/ex01/pyrmont/Response.java
@@ -15,53 +15,50 @@ import java.io.File;
 
 public class Response {
 
-  private static final int BUFFER_SIZE = 1024;
-  Request request;
-  OutputStream output;
+    private static final int BUFFER_SIZE = 1024;
+    Request request;
+    OutputStream output;
 
-  public Response(OutputStream output) {
-    this.output = output;
-  }
+    public Response(OutputStream output) {
+        this.output = output;
+    }
 
-  public void setRequest(Request request) {
-    this.request = request;
-  }
+    public void setRequest(Request request) {
+        this.request = request;
+    }
 
-  public void sendStaticResource() throws IOException {
-    byte[] bytes = new byte[BUFFER_SIZE];
-    FileInputStream fis = null;
-    try {
-      File file = new File(HttpServer.WEB_ROOT, request.getUri());
-      if (file.exists()) {
-        // 成功返回 html 文件时，需要正确设置返回结果的格式，否则浏览器会提示发送的响应无效
-        String okMessage = "HTTP/1.1 200 ok\r\n" +
-                "Content-Type: text/html\r\n" +
-                "\r\n";
-        output.write(okMessage.getBytes());
-        fis = new FileInputStream(file);
-        int ch = fis.read(bytes, 0, BUFFER_SIZE);
-        while (ch!=-1) {
-          output.write(bytes, 0, ch);
-          ch = fis.read(bytes, 0, BUFFER_SIZE);
+    public void sendStaticResource() throws IOException {
+        byte[] bytes = new byte[BUFFER_SIZE];
+        FileInputStream fis = null;
+        try {
+            File file = new File(HttpServer.WEB_ROOT, request.getUri());
+            if (file.exists()) {
+                // 成功返回 html 文件时，需要正确设置返回结果的格式，否则浏览器会提示发送的响应无效
+                String okMessage = "HTTP/1.1 200 ok\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "\r\n";
+                output.write(okMessage.getBytes());
+                fis = new FileInputStream(file);
+                int ch = fis.read(bytes, 0, BUFFER_SIZE);
+                while (ch != -1) {
+                    output.write(bytes, 0, ch);
+                    ch = fis.read(bytes, 0, BUFFER_SIZE);
+                }
+            } else {
+                // file not found
+                String errorMessage = "HTTP/1.1 404 File Not Found\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "Content-Length: 23\r\n" +
+                        "\r\n" +
+                        "<h1>File Not Found</h1>";
+                output.write(errorMessage.getBytes());
+            }
+        } catch (Exception e) {
+            // thrown if you cannot instantiate a File object
+            e.printStackTrace();
+        } finally {
+            if (fis != null)
+                fis.close();
         }
-      }
-      else {
-        // file not found
-        String errorMessage = "HTTP/1.1 404 File Not Found\r\n" +
-          "Content-Type: text/html\r\n" +
-          "Content-Length: 23\r\n" +
-          "\r\n" +
-          "<h1>File Not Found</h1>";
-        output.write(errorMessage.getBytes());
-      }
     }
-    catch (Exception e) {
-      // thrown if cannot instantiate a File object
-      System.out.println(e.toString() );
-    }
-    finally {
-      if (fis!=null)
-        fis.close();
-    }
-  }
 }
